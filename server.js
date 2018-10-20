8b6041d3a4badb44738013e17dbbff3e6bb923ba
Dickie Stevens <rsk0528@gmail.com>
2018-10-19 22:09:44 -0400

    heroku deployment

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 // Pulling our Note and Article.js models
-var Note = require("./models/Note.js");
-var Article = require("./models/Article.js");
+var Note = require("./models/note.js");
+var Article = require("./models/article.js");
 // Scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
