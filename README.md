03f93253a8ad4203ca36e4faf61e3f6888f4884f
Dickie Stevens <rsk0528@gmail.com>
2018-10-19 21:41:23 -0400

    GoComics Mongo Scraper

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # MongoScraper
+A web app that scrapes GoComics content. Made with Mongo and Cheerio.
+
+## Description
+
+This application is a full stack application built using Bootstrap/HTML/CSS/Javascript and the backend implemented with Node.js, Express, router, cheerio, and mongoose and handlebars.
+
+The user also has the option of saving an article to the database, add notes, and delete items they no longer want.
+
+## Image Demo
+#### Home Screen
+![alt text](./public/assets/images/demo/home.png)
+
+#### Scraper Screen
+![alt text](./public/assets/images/demo/scrape.png)
+
+#### Saved Articles Screen
+![alt text](./public/assets/images/demo/saved.png)
+
+#### Note Screen
+![alt text](./public/assets/images/demo/note.png)
+
+## Deployment
+
+[Heroku Deployment found here](https://go-comics-mongo-scraper.herokuapp.com/).
---
+++ server.js
@@ -0,0 +1,62 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+// Pulling our Note and Article.js models
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+// Scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+// Handlebars.
+var exphbs = require("express-handlebars");
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
+const PORT = process.env.PORT || 3000;
+// Initialize Express
+var app = express();
+
+//Using morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+
+// Serve static content
+app.use(express.static("public"));
+
+//Set Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/scraper_controller.js");
+
+app.use("/", routes);
+
+mongoose.connect(  
+	process.env.MONGODB_URI || "mongodb://localhost/mongoscraper",
+  {
+    useMongoClient: true
+  }
+);
+
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+// Listen on port 3000
+app.listen(PORT, function() {
+  console.log("App running on PORT " + PORT);
+});
